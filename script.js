f6ee7c35958c29e1b46af09ce024136de0503eab
Togepi <rafal.squirtlee@gmail.com>
2023-09-30 17:04:14 +0200

    v1.5.2
    Removed meta connect countdown since it already ended.

--- script.js
+++ script.js
@@ -24,60 +24,66 @@ function initializeFontSizeAndSlider() {
 
 initializeFontSizeAndSlider();
 
+//
+// <h4>Time until Meta Connect</h1>
+// <p id="countdownone"></p>
+// </div>
+//
+
 ////////////////////////////////////////////////
 
 function updateCountdown() {
-    const currentDate = new Date();
-    let endDate = new Date("2023-09-27T16:00:00Z");
-
-    let timeDiff = endDate - currentDate;
-
-    if (timeDiff <= 0) {
-        document.getElementById("countdownone").innerHTML = "Meta connect is here!";
-        return;
-    }
-
-    let seconds = Math.floor((timeDiff / 1000) % 60);
-    let minutes = Math.floor((timeDiff / 1000 / 60) % 60);
-    let hours = Math.floor((timeDiff / (1000 * 60 * 60)) % 24);
-    let days = Math.floor((timeDiff / (1000 * 60 * 60 * 24)) % 30);
-    let months = Math.floor((timeDiff / (1000 * 60 * 60 * 24 * 30)) % 12);
-
-    let countdown_parts = "";
-
-    let lastDeleted = false;
-    if (months !== 0) {
-        lastDeleted = false;
-        countdown_parts += months + (months === 1 ? " month " : " months ");
-    } else {
-        lastDeleted = true;
-    }
-
-    if (days !== 0 || !lastDeleted) {
-        lastDeleted = false;
-        countdown_parts += days + (days === 1 ? " day " : " days ");
-    }
-
-    if (hours !== 0 || !lastDeleted) {
-        lastDeleted = false;
-        countdown_parts += hours + (hours === 1 ? " hour " : " hours ");
-    }
-
-    if (minutes !== 0 || !lastDeleted) {
-        lastDeleted = false;
-        countdown_parts += minutes + (minutes === 1 ? " minute " : " minutes ");
-    }
-
-    if (seconds !== 0 || !lastDeleted) {
-        lastDeleted = false;
-        countdown_parts += seconds + (seconds === 1 ? " second" : " seconds");
-    }
-
-    document.getElementById("countdownone").innerHTML = countdown_parts;
+   const currentDate = new Date();
+//    let endDate = new Date("2023-09-27T16:00:00Z");
+//
+//    let timeDiff = endDate - currentDate;
+//
+//    if (timeDiff <= 0) {
+//        document.getElementById("countdownone").innerHTML = "Meta connect is here!";
+//        return;
+//    }
+//
+//    let seconds = Math.floor((timeDiff / 1000) % 60);
+//    let minutes = Math.floor((timeDiff / 1000 / 60) % 60);
+//    let hours = Math.floor((timeDiff / (1000 * 60 * 60)) % 24);
+//    let days = Math.floor((timeDiff / (1000 * 60 * 60 * 24)) % 30);
+//    let months = Math.floor((timeDiff / (1000 * 60 * 60 * 24 * 30)) % 12);
+//
+//    let countdown_parts = "";
+//
+//    let lastDeleted = false;
+//    if (months !== 0) {
+//        lastDeleted = false;
+//        countdown_parts += months + (months === 1 ? " month " : " months ");
+//    } else {
+//        lastDeleted = true;
+//    }
+//
+//    if (days !== 0 || !lastDeleted) {
+//        lastDeleted = false;
+//        countdown_parts += days + (days === 1 ? " day " : " days ");
+//    }
+//
+//    if (hours !== 0 || !lastDeleted) {
+//        lastDeleted = false;
+//        countdown_parts += hours + (hours === 1 ? " hour " : " hours ");
+//    }
+//
+//    if (minutes !== 0 || !lastDeleted) {
+//        lastDeleted = false;
+//        countdown_parts += minutes + (minutes === 1 ? " minute " : " minutes ");
+//    }
+//
+//    if (seconds !== 0 || !lastDeleted) {
+//        lastDeleted = false;
+//        countdown_parts += seconds + (seconds === 1 ? " second" : " seconds");
+//    }
+//
+//    document.getElementById("countdownone").innerHTML = countdown_parts;
 
     /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-    endDate = new Date("2023-10-10T00:00:00Z");
+    endDate = new Date("2023-10-09T22:00:00Z");
 
     timeDiff = endDate - currentDate;
 
